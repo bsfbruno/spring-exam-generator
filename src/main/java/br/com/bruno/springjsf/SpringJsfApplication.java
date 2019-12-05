2276afaefe60ff0277149a83b13fad3c8d7b7baf
Bruno <bruno44sf@gmail.com>
2019-12-04 21:35:49 -0300

    Project created

---
+++ src/main/java/br/com/bruno/springjsf/SpringJsfApplication.java
@@ -0,0 +1,13 @@
+package br.com.bruno.springjsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bruno/springjsf/SpringJsfApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.bruno.springjsf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJsfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
